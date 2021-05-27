d70e73bdd4d0a35e5f1721050f71c1f63318a3ac
Paula Morais <pmorais78@gmail.com>
2021-05-27 01:03:17 -0300

    Adicionando SchoolContext

---
+++ AspNetCore.MVC.EFC/Data/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCore.MVC.EFC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.MVC.EFC.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) 
+            : base(options){ }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Student>().ToTable("Student");
+            builder.Entity<Course>().ToTable("Course");
+            builder.Entity<Enrollment>().ToTable("Enrollment");
+        }
+    }
+
+
+
+}
