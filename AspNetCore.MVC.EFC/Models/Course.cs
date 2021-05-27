4cd3580798d3c1a58b94fec4b2ed9ec244ade706
Paula Morais <pmorais78@gmail.com>
2021-05-26 23:53:27 -0300

    Adicionado Models

---
+++ AspNetCore.MVC.EFC/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.MVC.EFC.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ AspNetCore.MVC.EFC/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCore.MVC.EFC.Models.Enum;
+
+namespace AspNetCore.MVC.EFC.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public Course CourseId { get; set; }
+        public Student StudanteId { get; set; }
+        public Grade? Grade { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
